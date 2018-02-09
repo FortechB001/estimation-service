1f52d9594f321f106b8acd190d02f6d6a7f914cf
Silviu <silviu.curuciuc@gmail.com>
2018-02-09 14:07:01 +0200

    Enable Eureka client

--- src/main/java/com/fortech/estimationservice/EstimationServiceApplication.java
+++ src/main/java/com/fortech/estimationservice/EstimationServiceApplication.java
@@ -2,7 +2,9 @@ package com.fortech.estimationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class EstimationServiceApplication {
 
